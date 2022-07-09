c8bde1d24be5666818460e7d458eaffe486d5bba
mmertcskn <mmert@sabanciuniv.edu>
2022-07-09 21:35:24 +0300

    inital commit

---
+++ src/test/java/com/coscom/couponservice/CouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coscom.couponservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
